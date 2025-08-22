a013102d35b38ffd07395aacb23bf4b8400735b9
minh nhan nguyen <82298632+bun781@users.noreply.github.com>
2025-08-23 02:13:28 +0900

    Update test_button.ino

--- machine/tests/test_button.ino
+++ machine/tests/test_button.ino
@@ -1,7 +1,11 @@
-const int checkButtonPin = 2;  // the number of the pushbutton pin
+#define checkButtonPin 25;  // the number of the pushbutton pin
 
 void setup() {
     Serial.begin(9600);
+    button_setup();
+}
+
+void button_setup() {
     pinMode(checkButtonPin, INPUT_PULLUP);
 }
 
